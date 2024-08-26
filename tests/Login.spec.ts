5345c55aac454b3f05ed05ffe0c973eb2493dff4
Chukwuemeka Uche <chukwuemeka.s.uche@gmail.com>
2024-08-26 22:25:37 +0100

    Implemented test for login functionality

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,11 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 const textName = "Form Authentication";
+const usernameField = "#username";
+const passwordField = "#password";
 
 test("Login page test", async ({ page }) => {
     await page.goto("https://the-internet.herokuapp.com/");
 
     await page.getByText(textName).click();
+
+    await page.locator(usernameField).fill("tomsmith");
+    await page.locator(passwordField).fill("SuperSecretPassword!");
+    await page.getByRole("button").click();
 
     await page.waitForTimeout(5000)
 })
